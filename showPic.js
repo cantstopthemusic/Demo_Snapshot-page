a2a8c1fcb7cfd84e436d03563840d2dd41c26822
stay'in alive <66505253+cantstopthemusic@users.noreply.github.com>
2020-07-23 21:33:52 +0800

    Add files via upload

---
+++ showPic.js
@@ -0,0 +1,12 @@
+function showPic(whichpic) {
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  var text = whichpic.getAttribute("title");
+  var description = document.getElementById("description");
+  placeholder.setAttribute("src", source);
+  description.firstChild.nodeValue = text;
+}
+function countBodyChildren() {
+  var body_elements = document.getElementsByTagName("body")[0];
+}
+window.onload = countBodyChildren;
