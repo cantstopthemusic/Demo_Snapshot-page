b35f324cbbc066325b22cd65cd601ec9abfdf83f
stay'in alive <66505253+cantstopthemusic@users.noreply.github.com>
2020-07-24 13:49:57 +0800

    Update showPic.js

--- showPic.js
+++ showPic.js
@@ -10,3 +10,21 @@ function countBodyChildren() {
   var body_elements = document.getElementsByTagName("body")[0];
 }
 window.onload = countBodyChildren;
+
+//popUp functions
+function popUp(winURL) {
+  window.open(winURL, "popup", "width=800,height=600");
+}
+
+function preparelinks() {
+  var links = document.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].getAttribute("class") == "popup") {
+      links[i].onclick = function () {
+        popUp(this.getAttribute("href"));
+        return false;
+      };
+    }
+  }
+}
+window.onload = preparelinks;
